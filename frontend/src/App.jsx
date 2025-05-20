3b2209eb27b1e00096351af835ad234ff204aaad
RichieRichJr <usagrich@aol.com>
2025-05-20 18:13:21 -0400

    Wrap CoachDashboard with EventProvider to fix context error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import CoachDashboard from "./pages/CoachDashboard.jsx";
+import { EventProvider } from "./context/EventContext";
 
 function App() {
-  return <CoachDashboard />;
+  return (
+    <EventProvider>
+      <CoachDashboard />
+    </EventProvider>
+  );
 }
 
 export default App;
