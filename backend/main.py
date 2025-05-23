076294aaa938a11babe6b271bdca543b2a239e21
RichieRichJr <usagrich@aol.com>
2025-05-23 08:17:12 -0400

    TEMP: Set CORS allow_origins to '*' for debugging CORS issues

--- backend/main.py
+++ backend/main.py
@@ -14,10 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://woo-combine.com",
-        "http://localhost:5173"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
