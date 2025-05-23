3a1212d1e1d1c6c2b2db12ea87370eb96c2b0526
RichieRichJr <usagrich@aol.com>
2025-05-22 23:54:50 -0400

    Chore: update alembic and db files, remove old migration scripts, cleanup main.py

--- alembic/env.py
+++ alembic/env.py
@@ -1,16 +1,13 @@
 from logging.config import fileConfig
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'backend')))
+from backend.models import Base
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-
-import sys
-import os
-from dotenv import load_dotenv
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'backend')))
-from backend.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -31,8 +28,6 @@ target_metadata = Base.metadata
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-
-load_dotenv(os.path.join(os.path.dirname(__file__), '..', 'backend', '.env'))
 
 db_url = os.environ.get('DATABASE_URL')
 if db_url:
