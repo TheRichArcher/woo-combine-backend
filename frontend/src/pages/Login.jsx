27c50cd3df5edb8c335c5cd2bd5731b46bd10bcb
RichieRichJr <usagrich@aol.com>
2025-05-25 23:03:26 -0400

    fix: Remove duplicate footer links on /login, keeping only in-card links for clean onboarding UX

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,19 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import WelcomeLayout from "../components/layouts/WelcomeLayout";
 import LoginForm from "../components/Welcome/LoginForm";
 
 export default function Login() {
-  const footerLinks = (
-    <div className="flex flex-col sm:flex-row gap-2 text-white/80 text-base">
-      <Link to="/signup" className="hover:underline">Don’t have an account? <span className="font-semibold text-white">Let’s Get Started</span></Link>
-      <span className="hidden sm:inline">&middot;</span>
-      <Link to="/forgot-password" className="hover:underline">Forgot password?</Link>
-    </div>
-  );
   return (
     <WelcomeLayout
-      footerLinks={footerLinks}
       contentClassName="min-h-[70vh]"
       hideHeader={true}
       showOverlay={false}
